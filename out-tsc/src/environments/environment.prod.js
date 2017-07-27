25e88c76e248fd0373ec1723af6b6d3ab86e08da
Sabri Aziri <azirisabri@gmail.com>
2017-07-27 13:39:53 +0200

    Auto-generated commit

--- out-tsc/src/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=C:/xampp/htdocs/ng2-validation-manager/src/src/environments/environment.prod.js.map
--- out-tsc/src/main.js
+++
@@ -1,9 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-if (environment.production) {
-    enableProdMode();
-}
-platformBrowserDynamic().bootstrapModule(AppModule);
-//# sourceMappingURL=C:/xampp/htdocs/ng2-validation-manager/src/src/main.js.map
